1eeb302139529fd2864ff6494b9e8979dd853135
kirill <kirillerohin@gmail.com>
2023-03-27 22:15:43 +0300

    fix: forgotten fix

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.engine import game_engine
+from brain_games.games.progression_game import game
 
 
 def main():
-    game_engine()
+    game_engine(game)
 
 
 if __name__ == '__main__':
