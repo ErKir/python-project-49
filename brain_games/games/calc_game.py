5710169fdeb37397929c1c960d7a99116d55fcfc
kirill <kirillerohin@gmail.com>
2023-03-30 18:58:47 +0300

    fix: add choice for operations

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -6,7 +6,11 @@ MAX_NUMBER = 20
 
 # math OPERATIONS
 OPERATIONS = ['-', '+', '*']
-
+ACTION = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+}
 
 RULES_OF_GAME = 'What is the result of the expression?'
 
@@ -20,5 +24,5 @@ def get_question_and_answer():
     numb2 = get_random_num(MIN_NUMBER, MAX_NUMBER)
     operation = OPERATIONS[get_random_num(0, 2)]
     question = f'{numb1} {operation} {numb2}'
-    answer = str(operator.add(numb1, numb2))
+    answer = str(ACTION[operation](numb1, numb2))
     return question, answer
